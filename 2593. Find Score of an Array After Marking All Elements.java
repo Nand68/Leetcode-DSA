6cd362e5a6a9ef594d66237fcff68aea2a0528a2
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2024-12-13 15:20:09 +0530

    Update 2593. Find Score of an Array After Marking All Elements.java

--- 2593. Find Score of an Array After Marking All Elements.java
+++ 2593. Find Score of an Array After Marking All Elements.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+// 1st approach
 class Solution {
     public long findScore(int[] nums) {
         long score = 0;
@@ -37,5 +37,43 @@ class Solution {
             this.value = value;
             this.index = index;
         }
+    }
+}
+
+
+// 2nd approach 
+
+
+class Solution {
+    public long findScore(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = nums[i];
+            dp[i][1] = i;
+        }
+
+        Arrays.sort(dp, (a, b) -> a[0] - b[0]);
+
+        boolean[] marked = new boolean[n];
+        long result = 0;
+
+        for (int[] arr : dp) {
+            int val = arr[0];
+            int ind = arr[1];
+
+            if (!marked[ind]) {
+                result += val;
+
+                marked[ind] = true;
+                if (ind > 0)
+                    marked[ind - 1] = true;
+                if (ind < n - 1) 
+                    marked[ind + 1] = true;
+            }
+        }
+
+        return result;
     }
 }
