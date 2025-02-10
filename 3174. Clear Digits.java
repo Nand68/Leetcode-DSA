0431abda9757b27750bead3f696042991a91d30b
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-02-10 15:35:25 +0530

    Create 3174. Clear Digits.java

---
+++ 3174. Clear Digits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        sb.reverse();
+        return sb.toString();
+        
+    }
+}
