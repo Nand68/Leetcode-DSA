54fa5851be6b5f0af0ef2660f8b26839d7103047
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-02-12 16:13:05 +0530

    Update 2342. Max Sum of a Pair With Equal Sum of Digits.java

--- 2342. Max Sum of a Pair With Equal Sum of Digits.java
+++ 2342. Max Sum of a Pair With Equal Sum of Digits.java
@@ -24,3 +24,31 @@ class Solution {
         return (max == 0)?-1:max;
     }
 }
+
+// better 
+class Solution {
+    public int maximumSum(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        int[] map = new int[82];
+        for(int i = 0; i < n; i++){
+            int total = 0;
+            int number = nums[i];
+            while ( number > 0){
+                total = total + (number % 10);
+                number = number / 10;
+            }
+            if ( map[total] != 0){
+                int maxForthisNumber = map[total];
+                int rem = maxForthisNumber + nums[i];
+                max = Math.max(max,rem);
+                int maxNowafter = Math.max(maxForthisNumber,nums[i]);
+                map[total] = maxNowafter;
+            }
+            else{
+                map[total] = nums[i];
+            }
+        }
+        return (max == 0)?-1:max;
+    }
+}
