d923040b13c7c674289f18bddf10fc25996317ec
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-02-23 15:41:27 +0530

    Create 889. Construct Binary Tree from Preorder and Postorder Traversal.java

---
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int n;
+    HashMap<Integer,Integer> map = new HashMap<>();
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        n = preorder.length;
+        for (int i = 0; i < n; i++){
+            map.put(postorder[i],i);
+        }
+        return helper(0,n-1,0,n-1,preorder,postorder);
+    }
+    public TreeNode helper(int i1,int i2, int j1,int j2,int[] preorder, int[] postorder){
+        if(i1 > i2 || j1 > j2 ){
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[i1]);
+        if ( i1 == i2){
+            return root;
+        }
+        int r = map.get(preorder[i1+1]);
+        int size = r-j1+1;
+        root.left = helper(i1+1,i1+size,j1,r,preorder,postorder);
+        root.right = helper(i1+size+1,i2,r+1,j2-1,preorder,postorder);
+        return root;
+
+    }
+}
